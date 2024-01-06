1e82015ab29abb7b4e7dc8439c4a76755d5ea0ca
unknown <vhisoares39@gmail.com>
2024-01-06 18:29:55 -0300

    fix: router

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
    
@@ -11,8 +12,8 @@ export const router = createBrowserRouter([
     children:[{path:'/', element:<Dashboard/>}]
    },
    {
-    path: '/sign-in',
-    element: <AppLayout/>,
-    children:[{path:'/', element:<SignIn/>}]
+    path: '/',
+    element: <AuthLayout/>,
+    children:[{path:'/sign-in', element:<SignIn/>}]
    }
 ])
