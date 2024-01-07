f87c33dc50ebe35aef8e396308422cbf6eb42440
unknown <vhisoares39@gmail.com>
2024-01-06 23:55:04 -0300

    Add: toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,38 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@radix-ui/react-label"
+
 import { Helmet } from "react-helmet-async"
+import {useForm} from 'react-hook-form'
+import { z } from 'zod'
+
+import { toast } from "sonner"
+
+const signInForm = z.object({
+    email: z.string().email()
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn(){
+    const { register,handleSubmit, formState:{ isSubmitting} } = useForm<SignInForm>()
+
+    
+    async function handleSignIn (data: SignInForm){
+        try{
+            await new Promise((resolve)=> setTimeout(resolve,2000))
+            console.log(data)
+            toast.success('Enviamos um link de autenticação para seu e-mail',{
+                action:{
+                    label:'Reenviar',
+                    onClick:()=>{handleSignIn(data)}
+                }
+            })        
+        }catch{
+            toast.error('Credenciais invalidas')
+        }
+    }
+
     return(
         <>
             <Helmet title="Login"/>
@@ -13,13 +42,13 @@ export function SignIn(){
                         <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
                         <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
                     </div>
-                    <form className="space-y-4">
+                    <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
                         <div className="space-y-2">
                             <Label htmlFor="email">Seu e-mail</Label>
-                            <Input id="email" type="email"/>
+                            <Input id="email" type="email" {...register('email')}/>
                         </div>
 
-                        <Button className="w-full" type="submit">Acessar painel</Button>
+                        <Button disabled={isSubmitting} className="w-full" type="submit">Acessar painel</Button>
                     </form>
                 </div>
             </div>
