245ad5634ff9f369a449b070cd3ed8f12dbaec70
unknown <vhisoares39@gmail.com>
2024-01-10 00:17:40 -0300

    Add:pagination layout

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -8,40 +8,40 @@ import {
    from "@/components/ui/table";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilter } from "./order-table-filter";
+import { Pagination } from "@/components/pagination";
 
 export function Orders(){
     return(
        <>
             <div className="flex flex-col gap-4 over">
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-            </div>
-            <div className="space-y-2.5">
-               <OrderTableFilter/>
-
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[140px]">Identificador</TableHead>
-                                <TableHead className="w-[180px]">Realizado</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead>Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {Array.from({length:10}).map((_,i)=>{
-                                return(
-                                    <OrderTableRow key={i}/>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
+                <div className="space-y-2.5">
+                <OrderTableFilter/>
+                    <div className="border rounded-md">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[64px]"></TableHead>
+                                    <TableHead className="w-[140px]">Identificador</TableHead>
+                                    <TableHead className="w-[180px]">Realizado</TableHead>
+                                    <TableHead className="w-[140px]">Status</TableHead>
+                                    <TableHead>Cliente</TableHead>
+                                    <TableHead className="w-[140px]">Total do pedido</TableHead>
+                                    <TableHead className="w-[164px]"></TableHead>
+                                    <TableHead className="w-[132px]"></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {Array.from({length:10}).map((_,i)=>{
+                                    return(
+                                        <OrderTableRow key={i}/>
+                                    )
+                                })}
+                            </TableBody>
+                        </Table>
+                    </div>
+                    <Pagination pageIndex={0} totalCount={105} perPage={10}/>
                 </div>
-
             </div>
        </>
    )
